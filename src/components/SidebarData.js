1e3a1aaff8eaa3f89df059bd675ab05b58e6a7c2
IndrajithVNair <indrajith.19ubc126@mcka.in>
2021-09-27 17:33:11 +0530

    sidebar without sub nav

---
+++ src/components/SidebarData.js
@@ -0,0 +1,60 @@
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import * as RiIcons from "react-icons/ri";
+
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
+    icon: <AiIcons.AiFillHome />,
+    cName: "nav-text",
+  },
+  {
+    title: "Reports",
+    path: "/reports",
+    icon: <IoIcons.IoIosPaper />,
+    cName: "nav-text",
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+
+    subNav: [
+      {
+        title: "Users",
+        path: "/overview/users",
+        icon: <IoIcons.IoIosPaper />,
+      },
+      {
+        title: "Revenue",
+        path: "/overview/revenue",
+        icon: <IoIcons.IoIosPaper />,
+      },
+    ],
+  },
+  {
+    title: "Products",
+    path: "/products",
+    icon: <FaIcons.FaCartPlus />,
+    cName: "nav-text",
+  },
+
+  {
+    title: "Team",
+    path: "/team",
+    icon: <IoIcons.IoMdPeople />,
+    cName: "nav-text",
+  },
+  {
+    title: "Messages",
+    path: "/messages",
+    icon: <FaIcons.FaEnvelopeOpenText />,
+    cName: "nav-text",
+  },
+  {
+    title: "Support",
+    path: "/support",
+    icon: <IoIcons.IoMdHelpCircle />,
+    cName: "nav-text",
+  },
+];
